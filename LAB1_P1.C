1c7d4192b160280e336a05b321ff31c49534dd73
Phani <psknfb@mail.missouri.edu>
2017-02-08 17:22:14 -0600

    code 1

--- LAB1_P1.C
+++ LAB1_P1.C
@@ -8,6 +8,6 @@ int main(void)
  float r;
  printf("enter the radius of circle: ");
  scanf("%f", &r);
- printf("\nArea of circle is: %f",(pi*pow(r,2)));
+ printf("\ncircumference of circle is: %f",(pi*2*r));
 return 0;
 }
